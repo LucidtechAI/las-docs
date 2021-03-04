43fa414a0edd2b7c553b2a369c2f54d440a73c7a
Roger Andersen <hexjelly@hexjelly.com>
2021-03-04 10:22:09 +0100

    image contain and fill

--- tutorials/simple-demo/frontend/src/SimpleImageViewer.tsx
+++ tutorials/simple-demo/frontend/src/SimpleImageViewer.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
+const containerStyle: CSSProperties = { width: '100%', height: '95vh' };
+const imgStyle: CSSProperties = { width: '100%', height: '100%', objectFit: 'contain', objectPosition: 'top' };
 type SimpleImageViewerProps = {
   doc: string;
 }
 const SimpleImageViewer = ({doc}: SimpleImageViewerProps) => {
   return (
-    <div>
-      <img src={doc} />
+    <div style={containerStyle}>
+      <img src={doc} style={imgStyle}/>
     </div>
   )
 }
