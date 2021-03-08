2bcef4f04682096af6993d6bf5f91db122bf167a
Roger Andersen <hexjelly@hexjelly.com>
2021-03-08 13:05:37 +0100

    deal with tiff pages

--- tutorials/simple-demo/frontend/src/TIFFViewer.tsx
+++ tutorials/simple-demo/frontend/src/TIFFViewer.tsx
@@ -1,19 +1,45 @@
-import React, { useMemo } from 'react';
+import React, { CanvasHTMLAttributes, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import UTIF from 'utif';
+import { debounce } from './utils';
+import styles from './TIFFViewer.module.css';
+
+type RGBData = {
+  data: Uint8Array;
+  width: number;
+  height: number;
+};
+
+type TIFFPageProps = RGBData & CanvasHTMLAttributes<HTMLCanvasElement>;
+const TIFFPage = ({ data, width, height, ...rest }: TIFFPageProps) => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  if (ref.current) {
+    const ctx = ref.current.getContext('2d');
+    const clampedArray = Uint8ClampedArray.from(data);
+    const imgData = new ImageData(clampedArray, width, height);
+    ctx?.putImageData(imgData, 0, 0);
+  }
+
+  return <canvas width={width} height={height} ref={ref} {...rest} />;
+};
 
 type TIFFViewerProps = {
   doc: string;
 };
 const TIFFViewer = ({ doc }: TIFFViewerProps): JSX.Element => {
-  const docBinary = useMemo(() => {
+  const [width, setWidth] = useState(600);
+
+  const docContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const pages = useMemo(() => {
     const binData = Uint8Array.from(atob(doc || ''), (c) => c.charCodeAt(0));
-    const decodedDocPages: Array<Uint8Array> = [];
+    const decodedDocPages: Array<RGBData> = [];
     try {
       const data = UTIF.decode(binData);
       for (const page of data) {
         UTIF.decodeImage(binData, page);
         const decodedImage = UTIF.toRGBA8(page);
-        decodedDocPages.push(decodedImage);
+        decodedDocPages.push({ data: decodedImage, width: page.width || 600, height: page.height || 600 * 1.33 });
       }
     } catch (e) {
       console.error(e);
@@ -22,8 +48,44 @@ const TIFFViewer = ({ doc }: TIFFViewerProps): JSX.Element => {
     }
   }, [doc]);
 
-  console.log(docBinary);
-  return <div>Test</div>;
+  useLayoutEffect(() => {
+    if (docContainerRef.current) {
+      const containerWidth = docContainerRef.current.getBoundingClientRect().width;
+      setWidth(containerWidth);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      if (docContainerRef.current) {
+        const containerWidth = docContainerRef.current.getBoundingClientRect().width;
+        setWidth(containerWidth);
+      }
+    }, 30);
+
+    window.addEventListener('resize', debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize);
+    };
+  }, []);
+
+  return (
+    <div ref={docContainerRef} className={styles.document}>
+      {pages.map((page, index) => {
+        return (
+          <TIFFPage
+            key={index}
+            width={page.width}
+            height={page.height}
+            data={page.data}
+            style={{ width, height: 'auto' }}
+            className={styles.page}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default TIFFViewer;
