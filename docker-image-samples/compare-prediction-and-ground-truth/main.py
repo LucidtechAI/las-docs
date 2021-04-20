fc897f894acee81def0c7ce3249f8816bc94dfcf
Magnus Rud <mag.aars@gmail.com>
2021-04-20 11:42:13 +0200

    clean up

--- docker-image-samples/compare-prediction-and-ground-truth/main.py
+++ docker-image-samples/compare-prediction-and-ground-truth/main.py
@@ -7,13 +7,6 @@ from las import Client
 
 logging.getLogger().setLevel(logging.INFO)
 
-def create_prediction(label, value, confidence):
-    return {
-        'label': label,
-        'value': value,
-        'confidence': confidence,
-    }
-
 
 def handler(las_client, event, environ):
     document_id = event['documentId']
@@ -24,24 +17,31 @@ def handler(las_client, event, environ):
     document = las_client.get_document(document_id=document_id)
     ground_truth = {field['label']: field['value'] for field in document['groundTruth']}
     enhanced_predictions = []
+    logging.info(f'Ground Truth: {ground_truth} ')
 
     for prediction in predictions:
-        ground_truth_value = ground_truth.pop(prediction['label'])
+        label = prediction['label']
+        ground_truth_value = ground_truth.pop(label)
         if prediction['value'] == ground_truth_value:
+            logging.info(f'Prediction and ground truth coincides: {label}={ground_truth_value}')
             prediction['confidence'] = 1.0
         elif prediction['confidence'] < confidence_threshold:
+            logging.info(f'Prediction and ground truth disagrees on {label}, use value from ground truth')
             prediction['confidence'] = ground_truth_confidence
             prediction['value'] = ground_truth_value
+        else:
+            logging.info(f'Prediction and ground truth disagrees on {label}, use value from prediction')
         enhanced_predictions.append(prediction)
 
     for label, value in ground_truth.items():
+        logging.info(f'Adding {label} from ground truth')
         enhanced_predictions.append({'label': label, 'value': value, 'confidence': ground_truth_confidence})
 
+    logging.info(f'new "prediction": {enhanced_predictions} ')
     # correction is needed unless all confidences are 1.0,
     # By setting the environment variable GROUND_TRUTH_CONFIDENCE < 1 you can choose to correct
     # values that either had no prediction or the prediction was lower than CONFIDENCE_THRESHOLD
     needs_correction = min([p['confidence'] for p in enhanced_predictions]) < 1.0
-
     event.update({
         'predictions': enhanced_predictions,
         'needsCorrection': needs_correction,
